9667e29c2d7224edefe98b44aef3e9827a887f10
IvanTomashevskiy <54989677+IvanTomashevskiy@users.noreply.github.com>
2019-09-29 18:00:11 +0300

    New

--- src/card.js
+++ src/card.js
@@ -1,79 +1,51 @@
-//import { Popup, Popupthird, Popupsecond } from '../scripts/popup.js';
 export default class Card {
     constructor(name, link) {
+        this.name = name;
+        this.link = link;
+        this.cardElement = this.create();
 
-        this.name = name
-        this.link = link
-        this.create(link)
-        this.element
-            .querySelector('.place-card__like-icon')
-            .addEventListener('click', this.like);
-        this.element
-            .querySelector('.place-card__delete-icon')
-            .addEventListener('click', this.remove);
-        this.element
-            .querySelector('.place-card__image')
-            .addEventListener('click', this.open);
+        this.likeHandler = this.like.bind(this);
+        this.deleteHandler = this.remove.bind(this);
+        
+        this.cardElement.querySelector('.place-card__like-icon').addEventListener('click', this.likeHandler);
+        this.cardElement.querySelector('.place-card__delete-icon').addEventListener('click', this.deleteHandler);
     }
-    like(event) {
-        console.log('!')
-        event.target.classList.toggle('place-card__like-icon_liked');
-    }
-    remove(event) {
-        placesList.removeChild(event.target.closest('.place-card'))
-        event.stopPropagation();
-
-    }
-    create(link) {  
+    create() { 
         const cardContainer = document.createElement('div');
         cardContainer.classList.add('place-card');
-
-        const deleteCardButton = document.createElement('button');
-        deleteCardButton.classList.add('place-card__delete-icon');
-
+    
+        const cardImage = document.createElement('div');
+        cardImage.classList.add('place-card__image');
+        cardImage.setAttribute('style', `background-image: url(${this.link})`);
+    
+        const cardDeleteButton = document.createElement('button');
+        cardDeleteButton.classList.add('place-card__delete-icon');
+    
         const cardDescription = document.createElement('div');
         cardDescription.classList.add('place-card__description');
-
+    
         const cardName = document.createElement('h3');
         cardName.classList.add('place-card__name');
-        cardName.textContent = this.name
-
-        const likeCardButton = document.createElement('button');
-        likeCardButton.classList.add('place-card__like-icon');
-
-
-        const cardBackground = document.createElement('div');
-        cardBackground.classList.add('place-card__image')
-        cardBackground.style.backgroundImage = `url(${this.link})`;
-
-
-        cardBackground.addEventListener('click', function () {
-            const imageBig = document.createElement('div');
-            imageBig.classList.add('image__big');
-            popupImgContent.appendChild(imageBig);
-
-            popupImg.classList.add('popup_is-opened')
-            const bigImg = document.createElement('div');
-            bigImg.classList.add('popup__bigimg');
-            imageBig.appendChild(bigImg);
-            bigImg.style.backgroundImage = `url(${link})`; 
-
-
-        });
-
-
-
-        cardContainer.appendChild(cardBackground);
-        cardBackground.appendChild(deleteCardButton);
-        cardDescription.appendChild(cardName);
-        cardDescription.appendChild(likeCardButton);
+        cardName.textContent = this.name; 
+    
+        const cardLikeButton = document.createElement('button');
+        cardLikeButton.classList.add('place-card__like-icon');
+    
+        cardContainer.appendChild(cardImage);
+        cardImage.appendChild(cardDeleteButton);
+    
         cardContainer.appendChild(cardDescription);
-
-
-        this.element = cardContainer;
+        cardDescription.appendChild(cardName);
+        cardDescription.appendChild(cardLikeButton);
+    
+        return cardContainer;
     }
-
-
+    like() {
+        this.cardElement.querySelector('.place-card__like-icon').classList.toggle('place-card__like-icon_liked');
+    }
+    remove() {
+        this.cardElement.querySelector('.place-card__like-icon').removeEventListener('click', this.likeHandler);
+        this.cardElement.querySelector('.place-card__delete-icon').removeEventListener('click', this.deleteHandler);
+        this.cardElement.remove();
+    }
 }
-
-
