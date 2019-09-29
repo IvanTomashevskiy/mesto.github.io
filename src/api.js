2b705ba5de91be9b8530054f6557390387452320
IvanTomashevskiy <54989677+IvanTomashevskiy@users.noreply.github.com>
2019-09-29 17:58:30 +0300

    New

--- src/api.js
+++ src/api.js
@@ -1,93 +1,59 @@
-//import avatar from '../imgages/avatar.jpg';
 export default class Api {
-    constructor(url, token) {
-        this.url = url;
-        this.token = token;
-    }
-    getInfo(callback) {
-        return fetch(`${this.url}/users/me`, {
-                method: 'GET',
-                headers: {
-                    authorization: this.token,
-                }
-            })
-            .then(res => {
-                if (res.ok) {
-                    return res.json()
-                }
-                return Promise.reject(res.status)
-            })
-            .then((data) => {
-                callback(data)
-            })
-            .catch((err) => {
-                console.log('Ошибка getInfo. Запрос не выполнен:', err)
-            })
+    constructor({baseUrl, headers}) {
+        this.url = baseUrl;
+        this.token = headers.authorization;
     }
 
-
-    getCards(callback) {
+    getInitialCards() {
         return fetch(`${this.url}/cards`, {
-                method: 'GET',
-                headers: {
-                    authorization: this.token
-                }
-            })
-            .then(res => {
-                if (res.ok) {
-                    return res.json()
-                }
-                return Promise.reject(res.status)
-            })
-            .then((data) => {    
-                    callback(data)
-            })
-            .catch((err) => {
-                console.log('Ошибка getCards. Запрос не выполнен:', err)
-            })
-    }
-
-    updateProfile(nik, about) { 
+            headers: {
+                authorization: this.token
+            }
+        })            
+            .then(res => this.checkStatus(res))                      
+    }  
+    getUserInfo() {
         return fetch(`${this.url}/users/me`, {
-                method: 'PATCH',
-                headers: {
-                    authorization: this.token,
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify({
-                    name: nik ,
-                    about: about
-                })
+            headers: {
+                authorization: this.token
+            }
+        })
+            .then(res => this.checkStatus(res)) 
+    }
+    changeUserInfo(name, about) {
+        return fetch(`${this.url}/users/me`, {
+            method: 'PATCH',
+            headers: {
+                authorization: this.token,
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                name,
+                about
             })
-            .then(res => {
-                if (res.ok) {
-                    return res.json()
-                }
-                return Promise.reject(res.status)
+        })
+            .then(res => this.checkStatus(res)) 
+    }
+    addCard(name, link) {
+        return fetch(`${this.url}/cards`, {
+            method: 'POST',
+            headers: {
+                authorization: this.token,
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                name: name,
+                link: link 
             })
-
-            .catch((err) => {
-                console.log('Ошибка profile. Запрос не выполнен:', err)
-            })
+        })
+            .then(res => this.checkStatus(res)) 
+    }
+    checkStatus(res) {
+        if(res.ok) {
+            return Promise.resolve(res.json());
+        }
+        return Promise.reject(res.status);
     } 
-    setUserInfo(name, about) {
-        return fetch(`${this.url}/users/me`, {
-                method: 'PATCH',
-                headers: {
-                    authorization: this.token,
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify({
-                     name, about 
-                })
-                // При совпадении ключей - значения можно не дублировать
-                // { name, about }
-            })
-            .then(res => {
-                if (res.ok) {
-                    return res.json();
-                }
-                return Promise.reject(`Ошибка: ${res.status}`);
-            })
-    }
+
+
 }
